4093e7ffd348090e4087dcb2ef6ebe80b8809f32
Dariusz Grzeszczak <d.grzeszczak@gmail.com>
2021-09-14 01:38:33 +0200

    - package

--- ReMVVMSampleSwiftUI/Modules/Package.swift
+++ ReMVVMSampleSwiftUI/Modules/Package.swift
@@ -17,13 +17,13 @@ let package = Package(
 
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "ReMVVMExt", path: "../../../ReMVVMExtSwiftUI"),
-        .package(name: "ReMVVM", path: "../../../ReMVVM")
-//        .package(
-//            name: "ReMVVMExt",
-//            url: "https://github.com/ReMVVM/ReMVVMExtSwiftUI",
-//            .branch("master")
-//        ),
+//        .package(name: "ReMVVMExt", path: "../../../ReMVVMExtSwiftUI"),
+//        .package(name: "ReMVVM", path: "../../../ReMVVM")
+        .package(
+            name: "ReMVVMExt",
+            url: "https://github.com/ReMVVM/ReMVVMExtSwiftUI",
+            .branch("master")
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
